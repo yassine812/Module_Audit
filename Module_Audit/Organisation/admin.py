5e6cab0f1e87c820925a6726b7d0ac125d851318
Yassine Zerni <zerniyassine21@gmail.com>
2026-02-27 16:26:52 +0100

    admin.py

--- Module_Audit/Organisation/admin.py
+++ Module_Audit/Organisation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (Section, Site, Processus, TypeEquipement,NiveauAttendu,Equipement,ProcessusDoc)
+admin.site.register(Section)
+admin.site.register(Site)
+admin.site.register(Processus)
+admin.site.register(TypeEquipement)
+admin.site.register(NiveauAttendu)
+admin.site.register(Equipement)
+admin.site.register(ProcessusDoc)
 
-# Register your models here.
--- Module_Audit/audit/admin.py
+++ Module_Audit/audit/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    TextRef,ChapitreNorme,Critere,
+    TypeCotation,TypePreuve,PreuveAttendu,
+    SousCritere,FormulaireAudit,TypeAudit,ListeAudit,
+    Cotation,FormulaireSousCritere,ResultatAudit,
+    SousCritereTypeAudit,DetailResultatAudit
+)
 
-# Register your models here.
+admin.site.register(TextRef)
+admin.site.register(ChapitreNorme)
+admin.site.register(Critere)
+admin.site.register(TypeCotation)
+admin.site.register(TypePreuve)
+admin.site.register(PreuveAttendu)
+admin.site.register(SousCritere)
+admin.site.register(FormulaireAudit)
+admin.site.register(TypeAudit)
+admin.site.register(ListeAudit)
+admin.site.register(Cotation)
+admin.site.register(FormulaireSousCritere)
+admin.site.register(ResultatAudit)
+admin.site.register(SousCritereTypeAudit)
+admin.site.register(DetailResultatAudit)
